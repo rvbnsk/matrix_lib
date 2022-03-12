436b9ddc2c2af9e505087db9554c6c46da6a8260
rvbnsk <58656716+rvbnsk@users.noreply.github.com>
2022-03-12 01:05:33 +0100

    added ctor and test operators overloading

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -16,10 +16,61 @@ class Matrix {
    public:
     Matrix();
     ~Matrix();
-    insert(const int &);
+    void insert(const int &);
+    Matrix operator+(const Matrix &);
+    // Matrix operator*(const Matrix &);
     friend std::ostream &operator<<(std::ostream &, const Matrix &);
 };
 
+template <typename T, std::size_t I, std::size_t J>
+Matrix<T, I, J>::Matrix()
+{
+    std::cout << "allocation" << std::endl;
+    array = new T *[I];
+    for (int i = 0; i < I; ++i) { array[i] = new T[J]; }
+}
+
+template <typename T, std::size_t I, std::size_t J>
+Matrix<T, I, J>::~Matrix()
+{
+    for (int i = 0; i < I; ++i) { delete[] array[i]; }
+    delete array;
+
+    array = nullptr;
+}
+
+template <typename T, std::size_t I, std::size_t J>
+void Matrix<T, I, J>::insert(const int &data)
+{
+    for (int i = 0; i < I; ++i) {
+        for (int j = 0; j < J; ++j) { this->array[i][j] = data; }
+    }
+}
+
+template <typename T, std::size_t I, std::size_t J>
+Matrix<T, I, J> Matrix<T, I, J>::operator+(const Matrix<T, I, J> &array)
+{
+    Matrix result;
+    for (auto i = 0; i < I; ++i) {
+        for (int j = 0; j < J; ++j) {
+            result.array[i][j] = this->array[i][j] + array.array[i][j];
+        }
+    }
+
+    return result;
+}
+
+template <typename T, std::size_t I, std::size_t J>
+std::ostream &operator<<(std::ostream &os, const Matrix<T, I, J> &array)
+{
+    for (int i = 0; i < I; ++i) {
+        for (int j = 0; j < J; ++j) { os << array.array[i][j] << " "; }
+        os << std::endl;
+    }
+
+    return os;
+}
+
 }  // namespace mtl
 
-#endif
+#endif  // MTL_MATRIX_HPP
