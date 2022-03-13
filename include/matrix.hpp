98f3e1ebb6f77d1fea7d788a9322deab612469fd
rvbnsk <maciekrybinski@yahoo.pl>
2022-03-13 22:55:30 +0100

    added more operator overloads and few methods

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -2,6 +2,7 @@
 #define MTL_MATRIX_HPP
 
 #include <iostream>
+#include <stdexcept>
 #include <type_traits>
 #include <utility>
 
@@ -22,13 +23,23 @@ class Matrix {
         -> Matrix<T, I, J> &;
     constexpr auto operator=(Matrix<T, I, J> &&) noexcept -> Matrix<T, I, J> &;
     void insert(const T &);
+    void sort();
     auto transpoze() -> Matrix<T, I, J> &;
+    auto power(const unsigned int &) -> Matrix<T, I, J> &;
+    auto det() -> T;
+    auto is_diagonal() -> bool;
 
     template <typename U, std::size_t A, std::size_t B>
     Matrix<T, I, J> operator+(const Matrix<U, A, B> &);
 
     template <typename U, std::size_t A, std::size_t B>
     Matrix<T, I, J> &operator+=(const Matrix<U, A, B> &);
+
+    template <typename U, std::size_t A, std::size_t B>
+    Matrix<T, I, J> operator-(const Matrix<U, A, B> &);
+
+    template <typename U, std::size_t A, std::size_t B>
+    Matrix<T, I, J> &operator-=(const Matrix<U, A, B> &);
 
     template <typename U, std::size_t A, std::size_t B>
     Matrix<T, I, J> operator*(const Matrix<U, A, B> &);
@@ -38,6 +49,7 @@ class Matrix {
 
     Matrix<T, I, J> operator*(const T &);
     Matrix<T, I, J> &operator*=(const T &);
+    Matrix<T, I, J> &operator^(const unsigned int &);
 
     template <typename U, std::size_t A, std::size_t B>
     inline auto operator==(const Matrix<U, A, B> &) -> bool;
@@ -72,6 +84,7 @@ constexpr auto Matrix<T, I, J>::operator=(Matrix<T, I, J> &&array) noexcept
     for (auto i = 0; i < I; ++i) {
         for (auto j = 0; j < J; ++j) { this->array[i][j] = array.array[i][j]; }
     }
+
     return *this;
 }
 
@@ -84,13 +97,81 @@ void Matrix<T, I, J>::insert(const T &data)
 }
 
 template <typename T, std::size_t I, std::size_t J>
+void Matrix<T, I, J>::sort()
+{
+}
+
+template <typename T, std::size_t I, std::size_t J>
 auto Matrix<T, I, J>::transpoze() -> Matrix<T, I, J> &
 {
     Matrix result;
     for (auto i = 0; i < I; ++i) {
         for (auto j = 0; j < J; ++j) { result.array[i][j] = this->array[i][j]; }
     }
+
     return result;
+}
+
+template <typename T, std::size_t I, std::size_t J>
+auto Matrix<T, I, J>::det() -> T
+{
+    if (I != J) throw std::range_error{"Matrix::det"};
+
+    T det = 0;
+    short sign = -1;
+
+    Matrix temp;
+
+    if (i == 1)
+        return this->array[0][0];
+    else if (i == 2)
+        return (
+            this->array[0][0] * this->array[1][1]
+            - this->array[0][1] * this->array[1][0]);
+    else {
+        for (auto m = 0; m < I; ++m) {
+            unsigned int temp_i = 0, temp_j = 0;
+            for (auto n = 1; n < I; ++n) {
+                for (auto o = 0; o < I; ++o) {
+                    if (o == m) continue;
+                    temp->array[temp_i][temp_j] = this->array[n][o];
+                    temp_j++;
+                }
+                if (temp_j == (i - 1)) {
+                    temp_i++;
+                    temp_j = 0;
+                }
+            }
+            sign = -sign;
+            det += (sign * this->array[0][m] * temp.det());
+        }
+    }
+
+    return det;
+}
+
+template <typename T, std::size_t I, std::size_t J>
+auto Matrix<T, I, J>::is_diagonal() -> bool
+{
+    if (I != J)
+        throw std::range_error("Matrix::diagonal");
+    else {
+        for (auto m = 0; m < I; ++m) {
+            for (auto n = 0; n < I; ++n) {
+                if (m != n && this->array[m][n] != 0) return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+template <typename T, std::size_t I, std::size_t J>
+auto Matrix<T, I, J>::power(const unsigned int &power) -> Matrix<T, I, J> &
+{
+    for (auto i = 0; i < (power - 1); ++i) { this->array *= this->array; }
+
+    return *this;
 }
 
 template <typename T, std::size_t I, std::size_t J>
@@ -122,6 +203,33 @@ Matrix<T, I, J> &Matrix<T, I, J>::operator+=(const Matrix<U, A, B> &array)
 
 template <typename T, std::size_t I, std::size_t J>
 template <typename U, std::size_t A, std::size_t B>
+Matrix<T, I, J> Matrix<T, I, J>::operator-(const Matrix<U, A, B> &array)
+{
+    Matrix result;
+    for (auto i = 0; i < I; ++i) {
+        for (auto j = 0; j < J; ++j) {
+            result.array[i][j] = this->array[i][j] - array.array[i][j];
+        }
+    }
+
+    return result;
+}
+
+template <typename T, std::size_t I, std::size_t J>
+template <typename U, std::size_t A, std::size_t B>
+Matrix<T, I, J> &Matrix<T, I, J>::operator-=(const Matrix<U, A, B> &array)
+{
+    for (auto i = 0; i < I; ++i) {
+        for (auto j = 0; j < J; ++j) {
+            this->array[i][j] = this->array[i][j] - array.array[i][j];
+        }
+    }
+
+    return *this;
+}
+
+template <typename T, std::size_t I, std::size_t J>
+template <typename U, std::size_t A, std::size_t B>
 Matrix<T, I, J> Matrix<T, I, J>::operator*(const Matrix<U, A, B> &array)
 {
     Matrix result;
@@ -133,6 +241,7 @@ Matrix<T, I, J> Matrix<T, I, J>::operator*(const Matrix<U, A, B> &array)
             }
         }
     }
+
     return result;
 }
 
@@ -161,6 +270,7 @@ Matrix<T, I, J> Matrix<T, I, J>::operator*(const T &scalar)
             result.array[i][j] = this->array[i][j] * scalar;
         }
     }
+
     return result;
 }
 
@@ -172,6 +282,15 @@ Matrix<T, I, J> &Matrix<T, I, J>::operator*=(const T &scalar)
             this->array[i][j] = this->array[i][j] * scalar;
         }
     }
+
+    return *this;
+}
+
+template <typename T, std::size_t I, std::size_t J>
+Matrix<T, I, J> &Matrix<T, I, J>::operator^(const unsigned int &power)
+{
+    for (auto i = 0; i < (power - 1); ++i) { this->array *= this->array; }
+
     return *this;
 }
 
@@ -184,6 +303,7 @@ inline auto Matrix<T, I, J>::operator==(const Matrix<U, A, B> &array) -> bool
             if (this->array[i][j] != array.array[i][j]) return false;
         }
     }
+
     return true;
 }
 
