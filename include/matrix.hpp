524df8ee9b23dc6037050a8241de184ebd7c0e1e
rvbnsk <58656716+rvbnsk@users.noreply.github.com>
2023-12-04 17:43:23 +0100

    Fix for copy assignment operator

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -411,12 +411,12 @@ template <detail::Arithmetic T, std::size_t I, std::size_t J>
 constexpr auto Matrix<T, I, J>::operator=(
     const Matrix<T, I, J>& matrix) noexcept -> Matrix<T, I, J>&
 {
-    size_ = matrix.size();
-    has_been_reallocated = matrix.is_reallocated();
-
-    alloc();
-
     if (&matrix != this) {
+        size_ = matrix.size();
+        has_been_reallocated = matrix.is_reallocated();
+
+        alloc();
+
         for (std::size_t i = 0; i < row_size(); ++i) {
             for (std::size_t j = 0; j < col_size(); ++j) {
                 data[i][j] = matrix.data[i][j];
